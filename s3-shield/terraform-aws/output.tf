4a3d7e0aac30d4f391073fcdef460b5960d9e428
ThijsFeryn <thijs@feryn.eu>
2025-10-07 12:23:38 +0200

    Renaming output variables accordingly

--- s3-shield/terraform-aws/output.tf
+++ s3-shield/terraform-aws/output.tf
@@ -1,7 +1,7 @@
 output "instance_public_ip_addr" {
-  value = aws_instance.tf-ve6.public_ip
+  value = aws_instance.tf_ve6.public_ip
 }
 
 output "instance_private_ip_addr" {
-  value = aws_instance.tf-ve6.private_ip
+  value = aws_instance.tf_ve6.private_ip
 }
